2e15122d70f13b8a670b9d65d9ba77beb82fc31e
Louise Naginski <louisindiana@gmail.com>
2023-10-06 15:44:19 -0400

    display key-value pairs of climb document whose id has been read

--- trk/src/Screens/ClimbDetail/index.js
+++ trk/src/Screens/ClimbDetail/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
 function ClimbDetail(props) {
+  const { climbData } = props.route.params;
   return (
     <View style={[styles.wrapper]} >
       <SafeAreaView/>
       <View style={[styles.center]}>
-        <Text>Climb Title</Text>
-        <Text>Climb Level</Text>
-        <Text>Climb Location</Text>
-        <Text>Climb RouteSetter</Text>
-        <Text>Climb Set On</Text>
+        {Object.entries(climbData).map(([key, value], index) => (
+          <View key={index} style={styles.row}>
+            <Text style={styles.keyText}>{key}</Text>
+            <Text style={styles.valueText}>{value}</Text>
+          </View>
+        ))}
+     
       </View>
     </View>
   )
@@ -19,12 +22,24 @@ function ClimbDetail(props) {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    padding: 15
+    padding: 15,
   }, 
   center: {
     alignItems: 'center',
      
-  }
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  keyText: {
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  valueText: {
+    flexShrink: 1, // To prevent text overflow
+  },
 })
 
 export default ClimbDetail;
