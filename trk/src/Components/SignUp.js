22c5e29f965a9989fd370e4ed78ca72c12d9568b
Louise Naginski <louisindiana@gmail.com>
2023-12-02 12:14:35 +0000

    added username field, made username required for account creation, changed the alert message to reflect addition of username, username gets added to user document

--- trk/src/Components/SignUp.js
+++ trk/src/Components/SignUp.js
@@ -7,11 +7,12 @@ const SignUp = ({ role, nyuComp }) => {
     console.log("[TEST] sign up call");
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
 
     const handleSignUp = () => {
         console.log("[TEST] sign up call");
-        if (!email.trim() || !password.trim()) {
-            Alert.alert('Error', 'Email and password must not be empty');
+        if (!email.trim() || !password.trim() || !username.trim()) {
+            Alert.alert('Error', 'Username, email and password must not be empty');
             return; // Early return to prevent Firebase call with empty fields
         }
         auth()
@@ -32,6 +33,7 @@ const SignUp = ({ role, nyuComp }) => {
                         taps: 0, // number of taps
                         nyuComp: nyuComp, // whether the user is a part of the NYU climbing competition
                         timestamp: new Date(),
+                        username: username,
                     })
                     .then(() => {
                         console.log('User added to Firestore');
@@ -50,6 +52,12 @@ const SignUp = ({ role, nyuComp }) => {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View style={styles.container}>
+                <TextInput
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                    style={styles.input}
+                />
                 <TextInput
                     placeholder="Email"
                     value={email}
