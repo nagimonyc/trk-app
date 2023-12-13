2bb39dadb87d84c94335593468d47754b5a0ee9e
Louise Naginski <louisindiana@gmail.com>
2023-12-13 12:27:12 -0500

    gymApi and logic for rendering the gym docs

---
+++ trk/src/api/GymsApi.js
@@ -0,0 +1,22 @@
+import firestore from '@react-native-firebase/firestore';
+
+
+function GymsApi() {
+
+  async function fetchGyms() {
+    try {
+      const snapshot = await firestore().collection('gyms').get();
+      return snapshot.docs.map(doc => ({ label: doc.data().Name, value: doc.id }));
+    } catch (error) {
+      console.error("Error fetching gyms: ", error);
+      return [];
+    }
+  }
+
+  return {
+    fetchGyms,
+  }
+}
+
+export default GymsApi
+
