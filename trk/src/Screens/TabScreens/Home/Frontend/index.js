a994658b659e248b9c69225727c06eb3c06e3f94
VGmt <Victor.guillemot@gmail.com>
2024-01-08 18:15:37 +0100

    tmp 1

--- trk/src/Screens/TabScreens/Home/Frontend/index.js
+++ trk/src/Screens/TabScreens/Home/Frontend/index.js
@@ -1,45 +1,58 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import NfcManager from 'react-native-nfc-manager';
 import AndroidPrompt from '../../../../Components/AndroidPrompt';
 import TapHistory from '../../../../Components/TapHistory';
 import useHomeScreenLogic from '../Backend/HomeScreenLogic';
 
 function HomeScreen(props) {
-  console.log('[TEST] HomeScreen called');
-  const {
-    renderNfcButtons,
-    climbsHistory,
-    androidPromptRef,
-    logo
-  } = useHomeScreenLogic(props);
-  return (
-    <>
-      <View style={[styles.wrapper, styles.center]}>
-
-        {renderNfcButtons()}
-        <View style={[styles.effortHistoryList, {}]}>
-          <Text style={{ fontSize: 25, fontWeight: '600', textAlign: 'left', marginLeft: 20, marginTop: 20, color: 'black' }}>Latest Gym Taps</Text>
-          <TapHistory climbsHistory={climbsHistory} fromHome={true} />
-        </View>
-      </View>
-      {(androidPromptRef) ? <AndroidPrompt ref={androidPromptRef} onCancelPress={() => NfcManager.cancelTechnologyRequest()} /> : null}
-    </>
-  );
+    console.log('[TEST] HomeScreen called');
+    const {
+        climbsHistory,
+        androidPromptRef,
+        logo
+    } = useHomeScreenLogic(props);
+    return (
+        <>
+            <View style={[styles.wrapper]}>
+                <View style={{ width: '100%' }}>
+                    <Text style={[styles.headerHome]}>Gym</Text>
+                    <View style={{ backgroundColor: '#0B1D4E' }}>
+                        <Image source={require('../../../../../assets/movementlicLogo.png')} style={{ width: '80%', height: 80, padding: 20 }} resizeMode="contain" />
+                    </View>
+                </View>
+                <View style={[styles.effortHistoryList, {}]}>
+                    <Text style={[styles.headerHome]}>Latest Gym Taps</Text>
+                    <TapHistory climbsHistory={climbsHistory} fromHome={true} />
+                </View>
+            </View >
+            {(androidPromptRef) ? <AndroidPrompt ref={androidPromptRef} onCancelPress={() => NfcManager.cancelTechnologyRequest()} /> : null
+            }
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-  center: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  effortHistoryList: {
-    flex: 1,
-    width: '100%',
-    color: 'black',
-  },
+    wrapper: {
+        flex: 1,
+    },
+    center: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerHome: {
+        fontSize: 25,
+        fontWeight: '600',
+        textAlign: 'left',
+        marginLeft: 20,
+        marginTop: 20,
+        marginBottom: 5,
+        color: 'black'
+    },
+    effortHistoryList: {
+        flex: 1,
+        width: '100%',
+        color: 'black',
+    },
 });
 
 export default HomeScreen;
