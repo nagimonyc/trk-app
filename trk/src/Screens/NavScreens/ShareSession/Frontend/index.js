108d385a06067501a14fe1cea9231773cd13afd5
abhipi <abhishekpillai94@gmail.com>
2024-02-03 21:04:45 -0500

    Permissions and Functionality to Save the Session Image Locally/ Share it

--- trk/src/Screens/NavScreens/ShareSession/Frontend/index.js
+++ trk/src/Screens/NavScreens/ShareSession/Frontend/index.js
@@ -1,24 +1,120 @@
 import React, { useRef } from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, SafeAreaView, CameraRoll } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { captureRef } from 'react-native-view-shot';
 import LinearGradient from 'react-native-linear-gradient';
-import RNFS from 'react-native-fs';
 import Toast from 'react-native-toast-message';
 import Share from 'react-native-share';
+import RNFS from 'react-native-fs';
+import { PermissionsAndroid} from "react-native";
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 
 function ShareView({ route }) {
     // Destructure climbData from route.params
     const { climbData } = route.params;
-
-    // Use climbData to store in variables
-    const { imageUrl, climbCount, grade } = climbData;
-
-    // Create a ref for the view you want to capture
+    const { imageUrl, climbCount, grade, duration } = climbData;
     const viewRef = useRef();
+
+    //Permission Check for Android
+    async function hasAndroidPermission() {
+        const getCheckPermissionPromise = () => {
+          if (Platform.Version >= 33) {
+            return Promise.all([
+              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
+              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+            ]).then(
+              ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+                hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+            );
+          } else {
+            return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+          }
+        };
+      
+        const hasPermission = await getCheckPermissionPromise();
+        if (hasPermission) {
+          return true;
+        }
+        const getRequestPermissionPromise = () => {
+          if (Platform.Version >= 33) {
+            return PermissionsAndroid.requestMultiple([
+              PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+              PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+            ]).then(
+              (statuses) =>
+                statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+                  PermissionsAndroid.RESULTS.GRANTED &&
+                statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+                  PermissionsAndroid.RESULTS.GRANTED,
+            );
+          } else {
+            return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+          }
+        };
+      
+        return await getRequestPermissionPromise();
+      }
+      
+      async function savePicture(tag) {
+        if (Platform.OS === "android" && !(await hasAndroidPermission())) {
+          return;
+        }
+        CameraRoll.save(tag, {type: 'photo'});
+      };
+
+    const saveView = async () => {
+        try {
+            const uri = await captureRef(viewRef, {
+                format: 'jpg',
+                quality: 0.8,
+            });
+
+            const fileName = `climb-share-${Date.now()}.jpg`.replace(':','-');
+            let filePath;
+            if (Platform.OS === 'android') {
+                filePath = `${RNFS.ExternalDirectoryPath}/${fileName}`;
+            } else {
+                filePath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+            }
+            //console.log(uri);
+            await RNFS.copyFile(uri, filePath);
+            return filePath;
+        } catch (error) {
+            console.error('Error saving the image: ', error);
+        }
+    };
+
+    const shareView = async () => {
+        try {
+            const filePath = await saveView();
+            if (filePath) {
+                await Share.open({
+                    url: `file://${filePath}`,
+                    type: 'image/jpeg',
+                });
+                Toast.show({ type: 'success', text1: 'Image loaded successfully!' });
+            }
+        } catch (error) {
+            Toast.show({ type: 'error', text1: 'Failed to load image!' });
+            console.error('Error sharing the image: ', error);
+        }
+    };
+
+    const saveImageLocally = async () => {
+        try {
+            const filePath = await saveView();
+            if (filePath) {
+                await savePicture(filePath);
+                Toast.show({ type: 'success', text1: 'Image saved successfully!' });
+            }
+        } catch (error) {
+            Toast.show({ type: 'error', text1: 'Failed to save image!' });
+            console.error('Error sharing the image: ', error);
+        }
+    };
 
     return (
         <View style={styles.container}>
-            <View ref={viewRef} style={styles.captureArea}>
+            <View style={styles.captureArea} ref={viewRef} collapsable={false}>
                 <Image source={{ uri: imageUrl }} style={styles.image} />
                 <Image
                     source={require('../../../../../assets/Nagimo-Logotype.png')}
@@ -26,7 +122,7 @@ function ShareView({ route }) {
                 />
                 <LinearGradient
                     style={styles.textOverlay}
-                    colors={['transparent', '#505050']} // Gradient from transparent to black
+                    colors={['transparent', '#505050']}
                     start={{ x: 0, y: 0 }}
                     end={{ x: 0, y: 1 }}>
                     <View style={{ flex: 1, alignSelf: 'flex-end', marginLeft: 10 }}>
@@ -34,8 +130,8 @@ function ShareView({ route }) {
                         <Text style={styles.overlayTextBig}>{climbCount}</Text>
                     </View>
                     <View style={{ flex: 1, alignSelf: 'flex-end' }}>
-                        {/* <Text style={styles.overlayText}>Time</Text> */}
-                        {/* <Text style={styles.overlayTextBig}>34m 52s</Text> */}
+                        <Text style={styles.overlayText}>Time</Text>
+                        <Text style={styles.overlayTextBig}>{duration}</Text>
                     </View>
                     <View style={{ flex: 1, alignSelf: 'flex-end', marginRight: 10 }}>
                         <Text style={styles.overlayText}>Last Climb</Text>
@@ -43,7 +139,14 @@ function ShareView({ route }) {
                     </View>
                 </LinearGradient>
             </View>
-            <View style={{ marginTop: 15 }}><Text>Feature in progress ... updates next week!</Text></View>
+            <View style={{display:'flex', flexDirection:'row'}}>
+                <TouchableOpacity style={styles.shareButton} onPress={saveImageLocally}>
+                    <Text style={styles.shareButtonText}>Save</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.shareButton} onPress={shareView}>
+                    <Text style={styles.shareButtonText}>Share</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -53,7 +156,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff', // Assuming a light theme
+        backgroundColor: '#fff',
     },
     captureArea: {
         width: '80%',
@@ -63,30 +166,15 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%',
-        position: 'absolute', // Make sure the image is positioned absolutely
+        position: 'absolute',
     },
-    textContainer: {
-        flex: 1,
-        justifyContent: 'space-around', // Distribute children evenly
-        alignItems: 'center', // Center children horizontally
-        padding: 16, // Add padding if needed
-    },
-    text: {
-        fontSize: 18, // Adjust your font size
-        color: '#000', // Adjust text color as needed
-        backgroundColor: 'rgba(255, 255, 255, 0.5)', // Semi-transparent background for legibility
-        padding: 8, // Add padding to the text
-        // More text styles...
-    },
-    shareButton: {
-        marginTop: 20,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: '#000',
-        borderRadius: 20,
-    },
-    shareButtonText: {
-        color: 'white',
+    logo: {
+        position: 'absolute',
+        top: -5,
+        right: 0,
+        width: 125,
+        height: 50,
+        resizeMode: 'contain',
     },
     textOverlay: {
         position: 'absolute',
@@ -97,24 +185,25 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
     },
     overlayText: {
-        color: 'white', // Text color for better contrast
-        fontSize: 14, // Adjust font size as needed
-        // Add any other styling for the text
         color: '#FFFEFF',
+        fontSize: 14,
         marginBottom: 5,
     },
     overlayTextBig: {
-        color: 'white', // Text color for better contrast
-        fontSize: 16, // Adjust font size as needed
+        color: '#FFFEFF',
+        fontSize: 16,
         fontWeight: 'bold',
     },
-    logo: {
-        position: 'absolute',
-        top: -5,
-        right: 0,
-        width: 125,
-        height: 50,
-        resizeMode: 'contain',
+    shareButton: {
+        marginTop: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#3498db',
+        borderRadius: 20,
+        marginRight: 10,
+    },
+    shareButtonText: {
+        color: 'white',
     },
 });
 
