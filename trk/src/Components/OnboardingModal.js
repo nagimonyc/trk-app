f9fa49c7dedb77758f12c7825293eadc2725a1e0
VGmt <Victor.guillemot@gmail.com>
2024-02-09 12:14:24 -0500

    onboarding gifs

--- trk/src/Components/OnboardingModal.js
+++ trk/src/Components/OnboardingModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, View, StyleSheet, Button, SafeAreaView, Text } from 'react-native';
+import { Modal, View, StyleSheet, Button, SafeAreaView, Text, Platform } from 'react-native';
 import Swiper from 'react-native-swiper';
 
 
@@ -9,6 +9,7 @@ import Slide2 from '../Screens/NavScreens/Onboarding/Front/slide2';
 import Slide3 from '../Screens/NavScreens/Onboarding/Front/slide3';
 import Slide4 from '../Screens/NavScreens/Onboarding/Front/slide4';
 import Slide5 from '../Screens/NavScreens/Onboarding/Front/slide5';
+import Slide6 from '../Screens/NavScreens/Onboarding/Front/slide6';
 import { platform } from 'process';
 
 // Add Slide5 if you have a fifth slide
@@ -25,8 +26,8 @@ const OnboardingModal = ({ isVisible, onClose }) => {
                 onRequestClose={onClose}>
                 <Swiper loop={false} showsButtons={true} activeDotColor="#FF8100" dotColor="#D9D9D9" nextButton={renderNextButton()} prevButton={renderPrevButton()}>
                     <Slide1 />
-                    <Slide2 />
-                    {platform !== 'ios' ? <Slide3 /> : <Slide5 />}
+                    {Platform.OS === 'ios' ? <Slide2 /> : <Slide6 />}
+                    {Platform.OS === 'ios' ? <Slide3 /> : <Slide5 />}
                     <Slide4 onClose={onClose} />
                 </Swiper>
             </Modal>
